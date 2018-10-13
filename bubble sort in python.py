f3c4d1296de39fc827eb136e6afb442136419714
abinovarghese <abinovarghese@gmail.com>
2018-10-13 20:10:18 +0530

    Python program with bubble sort

---
+++ bubble sort in python.py
@@ -0,0 +1,11 @@
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+alist = [10,2,32,12,323,11,1,7]
+bubbleSort(alist)
+print(alist)
