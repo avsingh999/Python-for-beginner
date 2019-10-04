3d16540003c8005f58c90987a0720f9d92ca5bad
musicViking <56165223+musicViking@users.noreply.github.com>
2019-10-04 21:50:58 +0530

    Create pycodemathlib.py

---
+++ pycodemathlib.py
@@ -0,0 +1,9 @@
+#start of code
+import math
+x=float(input("Enter a number: "))
+print('smallest integer greater than or equal to ',x,'=',math.ceil(x))
+print('argest integer less than or equal to',x,'=',math.floor(x))
+print('the cosine of',x,'=',math.cos(x))
+print('e to the power',x,'=',math.exp(x))
+print('the sqare root of',x,'=',math.sqrt(x))
+#end of code
